d8f7fe438491164a136cb076cbfc1899071f988d
Macbright007 <nbright239@gmail.com>
2022-05-06 03:34:50 +0100

    updated card code

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react'
-// import { data } from './Data'
+import { data } from './Data'
 import { CardContainer, InnerCard } from './styles'
 // import Typewriter from 'typewriter-effect';
 import Aos from "aos";
@@ -9,40 +9,7 @@ import "aos/dist/aos.css";
 const Card = () => {
   
 
-  const [Queries, SetQueries] = useState({
-    data: [
-      {
-          id: 1,
-          title: "Cindy",
-          content: " I love singing songs",
-          image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQttrw58jACgsV1I243pyvtJ5ft-uSIDN4aEb45JPR7ZPMBYej0iPAdkteC8ztTfDt8T7U&usqp=CAU"
-      },
-      {
-          id: 2,
-          title: "Mac",
-          content: " I love Bikes",
-          image: "https://wallpaperaccess.com/full/51366.jpg"
-      },
-      {
-          id: 3,
-          title: "Bright",
-          content: "Never ending Walk",
-          image: "https://wallpaperaccess.com/full/632198.jpg"
-      },
-      {
-          id: 4,
-          title: "Pharm Uzzy",
-          content: "Walking at the beach is therapeutical",
-          image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEJAM7MrNxJus4wGq2kA_jBrmcORPUb_V8khoHucI55u72-nTpiFEiDSSiTvGQp3KgfQs&usqp=CAU"
-      },
-      {
-          id: 5,
-          title: "Tayor",
-          content: "A path to Hope",
-          image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4FadTpHZBwi_eFBSYDA8g-oTSneXqzUzpb8VwK0L60UVC-WwkhBugB3h5X5_vPzipDqc&usqp=CAU"
-      }
-  ]
-  });
+  const [Queries, SetQueries] = useState(data);
   const [pageNum, SetPageNum] = useState(1);
 
   const observer = useRef(null);
@@ -56,42 +23,8 @@ const Card = () => {
 
   //
   useEffect(() => {
-    const newItems = Queries.data.concat(
-      [
-        {
-            id: 1,
-            title: "Cindy",
-            content: " I love singing songs",
-            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQttrw58jACgsV1I243pyvtJ5ft-uSIDN4aEb45JPR7ZPMBYej0iPAdkteC8ztTfDt8T7U&usqp=CAU"
-        },
-        {
-            id: 2,
-            title: "Mac",
-            content: " I love Bikes",
-            image: "https://wallpaperaccess.com/full/51366.jpg"
-        },
-        {
-            id: 3,
-            title: "Bright",
-            content: "Never ending Walk",
-            image: "https://wallpaperaccess.com/full/632198.jpg"
-        },
-        {
-            id: 4,
-            title: "Pharm Uzzy",
-            content: "Walking at the beach is therapeutical",
-            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEJAM7MrNxJus4wGq2kA_jBrmcORPUb_V8khoHucI55u72-nTpiFEiDSSiTvGQp3KgfQs&usqp=CAU"
-        },
-        {
-            id: 5,
-            title: "Tayor",
-            content: "A path to Hope",
-            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4FadTpHZBwi_eFBSYDA8g-oTSneXqzUzpb8VwK0L60UVC-WwkhBugB3h5X5_vPzipDqc&usqp=CAU"
-        }
-    ]);
-    SetQueries({
-      data: newItems,
-    });
+    const newItems = Queries.concat(data);
+    SetQueries( newItems);
   }, [pageNum])
 
 
@@ -111,7 +44,7 @@ const Card = () => {
   return(
     <div>
         {
-          Queries.data.map(query => {
+          Queries.map(query => {
             return(
               <CardContainer key={query.id}>
           <InnerCard>
